1f762eda0481797f868087e78d96b5e647bca220
Marius Retegan <marius.s.retegan@gmail.com>
2026-05-05 16:49:45 +0200

    Restructure the raw and processed data and conversion scripts

--- examples/manual/KEK_PFdata/convert_to_nexus.py
+++ examples/manual/KEK_PFdata/convert_to_nexus.py
@@ -4,47 +4,66 @@ from pathlib import Path
 import h5py
 import numpy
 from larch.xafs import pre_edge
-from larch.io import read_xdi, read_ascii, write_ascii
+from larch.io import read_ascii, write_ascii
 from larch.utils import gformat
 
 THIS_DIRECTORY = Path(__file__).parent
 
+PLANCK_HC = 12398.419843320027
+DEG2RAD = 0.017453292519943295
 
-def xdi2nexus(filename, nxroot, entry_name="entry"):
-    asc = read_ascii(filename)
-    dat = read_xdi(str(filename))
 
-    dat.energy
-    dat.mu = dat.mutrans * 1.0
-    dat.itrans = (dat.itrans * 100.0).astype("int32") / 100.0  # round
-    dat.array_labels[dat.array_labels.index("mutrans")] = "mu"
+def kekpf2nexus(filename, nxroot, entry_name="entry", metadata=None):
+
+    dat = read_ascii(filename, labels="angle_drive, angle_read, time, i0, ifluor")
+
+    meta = {}
+    if metadata is not None:
+        for kf, vf in metadata.items():
+            if kf not in meta:
+                meta[kf.lower()] = {}
+            for k, v in vf.items():
+                meta[kf.lower()][k.lower()] = v
+
+    monod = meta["monochromator"]["d_spacing"]
+
+    dat.energy = PLANCK_HC / (2 * monod * numpy.sin(DEG2RAD * (dat.data[0, :])))
+    dat.mu = dat.ifluor / dat.i0
 
     pre_edge(dat, pre1=-150, pre2=-50, norm1=150, norm2=750)
     pre_offset = dat.pre_edge_details.pre_offset
     pre_slope = dat.pre_edge_details.pre_slope
     nvict = dat.pre_edge_details.nvict
 
-    rawdata = numpy.zeros((len(dat.energy), 4), dtype="float64")
-    rawdata[:, 0] = dat.energy
-    rawdata[:, 1] = dat.norm
-    rawdata[:, 2] = (dat.itrans * 10.0).astype("int32") / 10.0
-    rawdata[:, 3] = dat.i0 * 1.0
+    coldata = []
+    coldata.append(dat.energy)
+    coldata.append(dat.norm * 1.0)
+    coldata.append(dat.ifluor * 1.0)
+    coldata.append(dat.i0 * 1.0)
+
+    array_labels = ["energy", "intensity", "ifluor", "i0"]
+    ndat, npts = dat.data.shape
+    for i in range(ndat):
+        label = dat.array_labels[i].lower()
+        if label not in array_labels:
+            array_labels.append(label)
+            coldata.append(dat.data[i, :])
 
     root = create_nxclass(nxroot, entry_name, "NXentry", default="plot")
     nxroot.attrs["default"] = entry_name
     root["definition"] = "NXxas"
 
-    root["mode"] = "transmission"
+    root["mode"] = "fluorescence"
 
     root["energy"] = dat.energy
     root["energy"].attrs["units"] = "eV"
     root["intensity"] = dat.norm
 
     element = create_nxclass(root, "element", "NXelement")
-    element["symbol"] = dat.attrs["element"]["symbol"]
+    element["symbol"] = meta["element"]["symbol"]
 
     edge = create_nxclass(root, "edge", "NXedge")
-    edge["name"] = dat.attrs["element"]["edge"]
+    edge["name"] = meta["element"]["edge"]
 
     plot = create_nxclass(root, "plot", "NXdata", signal="intensity", axes="energy")
     plot["energy"] = h5py.SoftLink(root["energy"].name)
@@ -57,13 +76,20 @@ def xdi2nexus(filename, nxroot, entry_name="entry"):
 
     # sample
     sample = create_nxclass(root, "sample", "NXsample")
-    for aname, value in dat.attrs["sample"].items():
+    for aname, value in meta["sample"].items():
         sample[aname] = value
         if aname.lower() == "name":
             names["sample"] = value
 
+    sample["prep"] = "unknown sample"
+
     # process
-    kwargs = ", ".join([f"{k}={v}" for k, v in dat.callargs.pre_edge.items()])
+    kwargs = []
+    for k, v in dat.callargs.pre_edge.items():
+        if v is not None:
+            kwargs.append(f"{k}={v}")
+
+    kwargs = ", ".join(kwargs)
 
     preline = f"pre_edge = {gformat(pre_offset, 12)} + {gformat(pre_slope, 12)}*energy"
     if nvict != 0:
@@ -72,11 +98,10 @@ def xdi2nexus(filename, nxroot, entry_name="entry"):
     proc = [
         "processing done with xraylarch version 0.9.80",
         f"larch.xafs.pre_edge(data, {kwargs})",
-        "using 'data' from arrays in rawdata",
-        "energy = data[:,0]      # column 1: energy (eV)",
-        "itrans = data[:, 2]     # column 3: transmitted beam intensity",
-        "i0 = data[:, 3]         # column 4: incident beam intensity",
-        "mu = -log(itrans/i0)    # absorbance",
+        "energy = col1    # column 1: energy (eV)",
+        "ifluor = col3    # column 3: fluorescence intensity",
+        "i0     = col4    # column 4: incident beam intensity",
+        "mu  = iflour/i0  ",
         f"edge_step = {gformat(dat.edge_step, 12)}",
         preline,
         "intensity = norm = (mu - pre_edge) /edge_step",
@@ -84,28 +109,35 @@ def xdi2nexus(filename, nxroot, entry_name="entry"):
 
     notes = "\n".join(proc)
 
-    header = [
-        "XDI/1.1",
-        "Column.1: energy eV",
-        "Column.2: norm",
-        "Column.3: itrans",
-        "Column.4: i0",
-    ]
+    header = ["XDI/1.1", "Column.1: energy eV", "Column.2: norm"]
+    collabel = ["energy          ", "norm            "]
+    for i, label in enumerate(array_labels[2:]):
+        header.append(f"Column.{i+3}: {label}")
+        collabel.append(label + " " * (1 + max(1, 15 - len(label))))
+
+    collabel = " ".join(collabel)
+
     end_header = []
     target = header
-    for j in asc.header[4:]:
+    for j in dat.header[4:]:
         if j.startswith("#"):
             j = j[1:].strip()
         if "---" in j:
             break
         if "///" in j:
             target = end_header
-        target.append(j)
+        if j.startswith("Mono."):
+            j = j.replace("Mono.", "Monochromator.")
+        if "Monochromator.dspa" in j:
+            j = j.replace("dspacing", "d_spacing")
+
+        if not j.startswith("Column."):
+            target.append(j)
 
     header.append("Process.program: xraylarch")
     header.append("Process.version: 0.9.80")
     header.append(f"Process.command: pre_edge(data, {kwargs})")
-    for i, p in enumerate(proc[3:]):
+    for i, p in enumerate(proc[2:]):
         header.append(f"Process.step{i+1:d}: {p}")
     for j in end_header:
         if j.startswith("#"):
@@ -121,22 +153,22 @@ def xdi2nexus(filename, nxroot, entry_name="entry"):
     nts["type"] = "text/plain"
 
     # rawdata
-    array_labels = json.dumps(["energy", "intensity", "itrans", "i0"])
-    root["rawdata"] = rawdata.T
-    root["rawdata"].attrs["array_labels"] = array_labels
+    root["rawdata"] = numpy.array(coldata).T
+    root["rawdata"].attrs["array_labels"] = json.dumps(array_labels)
 
     root["reference"] = "None"
 
     # scan
     scan = create_nxclass(root, "scan", "NXparameters")
-    scan["headers"] = json.dumps(dat.attrs)
-    scan["data_collector"] = "Matthew Newville"
+    scan["headers"] = json.dumps(meta)
+    scan["data_collector"] = "KEK PF BL9A"
     scan["filename"] = filename.name
 
-    for key, val in dat.attrs["scan"].items():
-        scan[key] = val
-    if getattr(dat, "comments", None) is not None:
-        scan["comments"] = dat.comments
+    if "scan" in meta:
+        for key, val in meta["scan"].items():
+            scan[key] = val
+        if getattr(dat, "comments", None) is not None:
+            scan["comments"] = dat.comments
 
     inst = create_nxclass(root, "instrument", "NXinstrument")
 
@@ -144,8 +176,8 @@ def xdi2nexus(filename, nxroot, entry_name="entry"):
     source["type"] = "Synchrotron X-ray Source"
     source["probe"] = "x-ray"
 
-    if "facility" in dat.attrs:
-        for aname, value in dat.attrs["facility"].items():
+    if "facility" in meta:
+        for aname, value in meta["facility"].items():
             if aname.lower() == "energy":
                 words = value.split(maxsplit=1)
                 en_value = float(words[0])
@@ -157,11 +189,12 @@ def xdi2nexus(filename, nxroot, entry_name="entry"):
                 if aname.lower() == "name":
                     names["facility"] = value
 
-    bl = create_nxclass(inst, "beamline", "NXparameters")
-    for aname, value in dat.attrs["beamline"].items():
-        bl[aname] = value
-        if aname.lower() == "name":
-            names["beamline"] = value
+    if "beamline" in meta:
+        bl = create_nxclass(inst, "beamline", "NXparameters")
+        for aname, value in meta["beamline"].items():
+            bl[aname] = value
+            if aname.lower() == "name":
+                names["beamline"] = value
 
     title_words = [
         names.get("sample", filename.name),
@@ -171,8 +204,9 @@ def xdi2nexus(filename, nxroot, entry_name="entry"):
     root["title"] = (" ".join(title_words)).strip()
 
     # mono
-    d_spacing = dat.attrs["mono"].get("d_spacing", 3.13550)
-    mono_name = dat.attrs["mono"].get("name", "Si 111")
+    mono_meta = meta.get("monochromator", meta.get("mono", {}))
+    d_spacing = mono_meta.get("d_spacing", 3.13551)
+    mono_name = mono_meta.get("name", "Si 111")
 
     def parse_mono_string(mono_name):
         mono_type = "Si"
@@ -208,22 +242,15 @@ def xdi2nexus(filename, nxroot, entry_name="entry"):
 
     i0 = create_nxclass(inst, "i0", "NXdetector")
     i0["data"] = dat.i0
-    i0["description"] = dat.attrs["detector"]["i0"]
+    i0["description"] = "Ion Chamber"
 
-    itrans = create_nxclass(inst, "itrans", "NXdetector")
-    itrans["data"] = dat.itrans
-    itrans["description"] = dat.attrs["detector"]["i1"]
+    ifluor = create_nxclass(inst, "ifluor", "NXdetector")
+    ifluor["data"] = dat.ifluor
+    ifluor["description"] = "Ion Chamber"
 
-    outfile = filename.parent / (filename.stem + "_new.xdi")
-    write_ascii(
-        outfile,
-        dat.energy,
-        dat.norm,
-        dat.itrans,
-        dat.i0,
-        header=header,
-        label="energy           norm             itrans           i0",
-    )
+    outfile = filename.parent / (filename.stem + "_new" + filename.suffix)
+
+    write_ascii(outfile, *coldata, header=header, label=collabel)
 
     print(f"done. Wrote group {entry_name} in file {outfile}")
 
@@ -239,10 +266,22 @@ def create_nxclass(root, name, nx_class, **attrs):
 
 def main(output_filename):
     with h5py.File(output_filename, "w") as nxroot:
-        xdi2nexus(THIS_DIRECTORY / "fe_metal_rt.xdi", nxroot, entry_name="fe_metal")
-        xdi2nexus(THIS_DIRECTORY / "fe2o3_rt.xdi", nxroot, entry_name="fe2o3")
-        xdi2nexus(THIS_DIRECTORY / "feo_rt1.xdi", nxroot, entry_name="feo")
+
+        metadata = {
+            "sample": {"name": "fe010"},
+            "element": {"symbol": "Fe", "edge": "K"},
+            "monochromator": {"crystal": "Si 111", "d_spacing": 3.1551},
+            "facility": {"name": "Photon Factory", "beamline": "BL9A"},
+            "detector": {"i0": "20 cm, He", "ifluor": "Vortex ME-4"},
+        }
+
+        kekpf2nexus(
+            THIS_DIRECTORY / "PF9A_2022.dat",
+            nxroot,
+            entry_name="fe010",
+            metadata=metadata,
+        )
 
 
 if __name__ == "__main__":
-    main(THIS_DIRECTORY / ".." / ".." / "nxxas_examples" / f"{THIS_DIRECTORY.name}.h5")
+    main(THIS_DIRECTORY / "converted.nx")
